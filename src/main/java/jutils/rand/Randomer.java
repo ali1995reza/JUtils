144bb331a2bc37ca4faead59c3b45313ad530288
Alireza Akhoundi <alirezaakhoundi73@gmail.com>
2021-08-21 23:39:59 +0430

    better Randomer implemented

--- src/main/java/jutils/rand/Randomer.java
+++ src/main/java/jutils/rand/Randomer.java
@@ -4,22 +4,44 @@ import jutils.assertion.Assertion;
 
 import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Random;
 
 public class Randomer<T> {
 
+    private static class RandomObjectHolder<T> {
+        private final T object;
+        private int getCount = 0;
+
+        private RandomObjectHolder(T object) {
+            this.object = object;
+        }
+
+        void increaseGetCount() {
+            ++getCount;
+        }
+
+        int getCount() {
+            return getCount;
+        }
+
+    }
+
     private final Random random = new SecureRandom();
-    private final List<T> objects;
+    private final List<RandomObjectHolder<T>> objects;
 
     public Randomer(List<T> objects) {
-        this.objects = new ArrayList<>(objects);
+        this.objects = new ArrayList<>();
+        for(T object : objects) {
+            this.objects.add(new RandomObjectHolder<>(object));
+        }
     }
 
     public Randomer(T ... objects) {
         this.objects = new ArrayList<>();
         for(T object:objects) {
-            this.objects.add(object);
+            this.objects.add(new RandomObjectHolder<>(object));
         }
     }
 
@@ -31,17 +53,30 @@ public class Randomer<T> {
         return remainingObjects()>0;
     }
 
-    public T getRandomObjectAndRemove() {
+    public T getRandomObjectAndRemove(int count) {
         synchronized (objects) {
             Assertion.ifTrue("objects are empty", objects.isEmpty());
-            return objects.remove(random.nextInt(objects.size()));
+            int rand = random.nextInt(objects.size());
+            RandomObjectHolder<T> object = objects.get(rand);
+            object.increaseGetCount();
+            if(object.getCount()>=count) {
+                objects.remove(rand);
+            }
+            return object.object;
         }
+    }
+
+    public T getRandomObjectAndRemove() {
+        return getRandomObjectAndRemove(1);
     }
 
     public T getRandomObject() {
         synchronized (objects) {
             Assertion.ifTrue("objects are empty", objects.isEmpty());
-            return objects.get(random.nextInt(objects.size()));
+            int rand = random.nextInt(objects.size());
+            RandomObjectHolder<T> object = objects.get(rand);
+            object.increaseGetCount();
+            return object.object;
         }
     }
 
